4cabff2618fbf213905dbd52c74929fa3b0541b4
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-08-29 16:03:41 +0100

    Created policty implementation

---
+++ policy-implementation.php
@@ -0,0 +1,97 @@
+<?php include_once('template/head.php'); ?>
+</head>
+<body class="page-template-default page wp-custom-logo wpb-js-composer js-comp-ver-5.4.5 vc_responsive">
+<div id="wrapper">
+    <?php include_once('template/header.php'); ?>
+    <div class="header-background header-background--single" data-slideshow-speed="10">
+        <div class="header-background__image header-background__image--default"
+             style="background-image: url('images/header-01.2.jpg'); "></div>
+    </div>
+    <div id="core">
+        <div class="core__inner">
+            <div id="columns">
+                <div class="columns__inner">
+                    <div class="lsvr-container">
+                        <div class="lsvr-grid">
+                            <div class="columns__main lsvr-grid__col lsvr-grid__col--span-6 lsvr-grid__col--push-3">
+                                <main id="main">
+                                    <div class="main__inner">
+                                        <div class="post-216 page type-page status-publish hentry">
+                                            <header class="main__header"><h1 class="main__title">Policy
+                                                    Implementation</h1>
+                                            </header>
+                                            <div class="page__content">
+                                                <div class="c-content-box">
+                                                    <h2>Policy implementation strategies</h2>
+                                                    <ul>
+                                                        <li>The Primary and Secondary education sub-sector is critical
+                                                            to the development of sound education in the State. It is
+                                                            the foundation on which subsequent educational blocks are
+                                                            laid. It is therefore important that this stage of the
+                                                            education system is solid.
+                                                        </li>
+                                                        <li>Over the years, the Ministry has adopted various strategies
+                                                            to ensure that laid down policies and philosophies on
+                                                            education are in tandem with the National Policy on
+                                                            Education and that other policy environments are well
+                                                            articulated and implemented. These include:
+                                                        </li>
+                                                        <li>Provision of efficient Quality Control and Assurance
+                                                            mechanisms through established statutory structures designed
+                                                            not only to ensure quality, but improved education service
+                                                            delivery to the citizenry. The structures include nine (9)
+                                                            Departments as stated here-under:
+                                                        </li>
+                                                    </ul>
+                                                    <ul style="font-weight: bold">
+                                                        <li>Administration</li>
+                                                        <li>Planning, Research and Statistics</li>
+                                                        <li>Schools</li>
+                                                        <li>Inspectorate</li>
+                                                        <li>Science, Vocational and Technical Education</li>
+                                                        <li>Examinations and Standards</li>
+                                                        <li>Finance and Accounts</li>
+                                                        <li>Educational Resource Centre</li>
+                                                        <li>Public Private Partnership</li>
+                                                    </ul>
+
+                                                    <ul>
+                                                        <li>Adoption of deliberate policies to attract and retain all
+                                                            children of school age through the provision of Tuition –
+                                                            Free and child-friendly school environment from Pre-Primary
+                                                            to Secondary levels of education.
+                                                        </li>
+                                                        <li>Intensified community participation in the daily
+                                                            administration and welfare of schools through the
+                                                            establishment of virile Parents’ – Teachers’ Association
+                                                            (PTA), School Mentoring Programme (SMP) as well as the
+                                                            School Based Management Committees (SBMCs).
+                                                        </li>
+                                                        <li>Public Private Partnership Initiative, involving the active
+                                                            participation of Corporate Bodies, NGOs, Philanthropists and
+                                                            International Development Partners in meeting specific areas
+                                                            of needs in schools, especially on infrastructural and
+                                                            capacity building development.
+                                                        </li>
+                                                    </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </main>
+                            </div>
+                            <?php include_once('template/left-sidebar.php'); ?>
+                            <?php include_once('template/right-sidebar.php'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once('template/footer.php'); ?>
+</div>
+<script type="text/javascript" src="js/3/7e7e4.js"></script>
+<script type="text/javascript" src="js/3/b8041.js"></script>
+<script type="text/javascript" src="js/3/d9a09.js"></script>
+<script type="text/javascript" src="js/3/a8cb8.js"></script>
+</body>
+</html>
