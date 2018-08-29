32ad1da9e6d67c69285a1446d02c598607e72e53
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-08-29 16:49:19 +0100

    created achievements and board's page

---
+++ achievements.php
@@ -0,0 +1,48 @@
+<?php include_once('template/head.php'); ?>
+</head>
+<body class="page-template-default page wp-custom-logo wpb-js-composer js-comp-ver-5.4.5 vc_responsive">
+<div id="wrapper">
+    <?php include_once('template/header.php'); ?>
+    <div class="header-background header-background--single" data-slideshow-speed="10">
+        <div class="header-background__image header-background__image--default"
+             style="background-image: url('images/header-01.2.jpg'); "></div>
+    </div>
+    <div id="core">
+        <div class="core__inner">
+            <div id="columns">
+                <div class="columns__inner">
+                    <div class="lsvr-container">
+                        <div class="lsvr-grid">
+                            <div class="columns__main lsvr-grid__col lsvr-grid__col--span-6 lsvr-grid__col--push-3">
+                                <main id="main">
+                                    <div class="main__inner">
+                                        <div class="post-216 page type-page status-publish hentry">
+                                            <header class="main__header"><h1 class="main__title">Achievements</h1>
+                                            </header>
+                                            <div class="page__content">
+                                                <div class="c-content-box">
+                                                    <h2>
+                                                            Photo Speaks
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </main>
+                            </div>
+                            <?php include_once('template/left-sidebar.php'); ?>
+                            <?php include_once('template/right-sidebar.php'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once('template/footer.php'); ?>
+</div>
+<script type="text/javascript" src="js/3/7e7e4.js"></script>
+<script type="text/javascript" src="js/3/b8041.js"></script>
+<script type="text/javascript" src="js/3/d9a09.js"></script>
+<script type="text/javascript" src="js/3/a8cb8.js"></script>
+</body>
+</html>
---
+++ boards.php
@@ -0,0 +1,60 @@
+<?php include_once('template/head.php'); ?>
+</head>
+<body class="page-template-default page wp-custom-logo wpb-js-composer js-comp-ver-5.4.5 vc_responsive">
+<div id="wrapper">
+    <?php include_once('template/header.php'); ?>
+    <div class="header-background header-background--single" data-slideshow-speed="10">
+        <div class="header-background__image header-background__image--default"
+             style="background-image: url('images/header-01.2.jpg'); "></div>
+    </div>
+    <div id="core">
+        <div class="core__inner">
+            <div id="columns">
+                <div class="columns__inner">
+                    <div class="lsvr-container">
+                        <div class="lsvr-grid">
+                            <div class="columns__main lsvr-grid__col lsvr-grid__col--span-6 lsvr-grid__col--push-3">
+                                <main id="main">
+                                    <div class="main__inner">
+                                        <div class="post-216 page type-page status-publish hentry">
+                                            <header class="main__header"><h1 class="main__title">Board</h1>
+                                            </header>
+                                            <div class="page__content">
+                                                <div class="c-content-box">
+                                                    <h2>Boards & Agencies</h2>
+                                                    <p>The Ministry has a Field Office in each of the 25 Local
+                                                        Government Areas of the State with a Chief Inspector of
+                                                        Education (CIE) overseeing the activities of education. In
+                                                        addition, there are three (3) statutory Boards, an Agency and a
+                                                        French Language Centre supervised by the Ministry, namely:</p>
+                                                    <ul>
+                                                        <li>Post Primary Education Board (PPEB)</li>
+                                                        <li>State Universal Basic Education Board (SUBEB)</li>
+                                                        <li>Technical & Vocational Education Board (TVEB)</li>
+                                                    </ul>
+                                                    <h2>Agency</h2>
+                                                    <p>Agency for Adult and Non-Formal Education</p>
+                                                    <h2>Centre</h2>
+                                                    <p> Christie Akako French Language Centre</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </main>
+                            </div>
+                            <?php include_once('template/left-sidebar.php'); ?>
+                            <?php include_once('template/right-sidebar.php'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once('template/footer.php'); ?>
+</div>
+<script type="text/javascript" src="js/3/7e7e4.js"></script>
+<script type="text/javascript" src="js/3/b8041.js"></script>
+<script type="text/javascript" src="js/3/d9a09.js"></script>
+<script type="text/javascript" src="js/3/a8cb8.js"></script>
+</body>
+</html>
