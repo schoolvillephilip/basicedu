d85b4b61a7e33628a68a65c5aa35f37e97a4087b
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-08-30 12:01:25 +0100

    Right Sidebar

--- template/head.php
+++ template/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ministry of Basic Education</title>
+    <title>DELTA STATE MINISTY OF EDUCATION (BASIC & SECONDARY)</title>
     <link rel='dns-prefetch' href='http://fonts.googleapis.com/'/>
     <link rel='stylesheet' id='bbp-default-css'
           href='css/bbpress01c4.css' type='text/css'
